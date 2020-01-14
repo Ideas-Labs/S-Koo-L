a39f74bdb0822fba7a83863da3105a3722079241
Parth Shandilya <parth1989shandilya@gmail.com>
2020-01-14 22:39:15 +0100

    Added Model for School Added Model for Subject Added Model for Teacher Added Model for Admin

---
+++ api/modules/school/models/school.py
@@ -0,0 +1,24 @@
+from main import db
+
+class School(db.Model):
+    __tablename__ = "school"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    name = db.Column(
+        db.String,
+        nullable=False
+    )
+
+    address = db.Column(
+        db.String,
+        nullable=False
+    )
+
+    affialiation = db.Column(
+        db.String,
+        nullable=False
+    )
